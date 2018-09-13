144b4212904b917587ee7b6cd2dcc839c35ea067
Metotron <metotron1@yahoo.com>
2018-09-14 02:52:31 +0300

    Шкода без слайдера картинок

---
+++ skoda/js/skoda.js
@@ -0,0 +1,54 @@
+function onYouTubeIframeAPIReady() {
+	videoSlider.init()
+}
+
+var videoSlider = {
+	videos: ['WiE7mtC7Hqg', 'g3N0QVZ8Ttw', 'oewRadlyrHo'],
+	activeVideo: 0,
+	videoPlayer: null,
+
+	init: function() {
+		var leftArrow = document.querySelector('.videoslider .arrow.left')
+		var rightArrow = document.querySelector('.videoslider .arrow.right')
+		var videoPoints = document.querySelector('.videopoints')
+		var slider = this
+
+		leftArrow.addEventListener('click', function() { slider.slideTo(slider.activeVideo - 1) }, false)
+		rightArrow.addEventListener('click', function() { slider.slideTo(slider.activeVideo + 1) }, false)
+		
+		for (var pnt = 0; pnt < this.videos.length; ++pnt)
+			videoPoints.innerHTML += '<div class="point"></div>'
+
+		this.createPlayer()
+	},
+
+	slideTo: function(index) {
+		if (index < 0) index = this.videos.length - 1
+		if (index >= this.videos.length) index = 0
+
+		this.activeVideo = index
+		this.removePlayer()
+		this.createPlayer()
+	},
+
+	removePlayer: function() {
+		document.querySelector('.ipad iframe').remove()
+		document.querySelector('.ipad').innerHTML = '<div id="videoplayer"></div>'
+	},
+
+	createPlayer: function() {
+		var videoPoints = document.querySelector('.videopoints')
+		var activePoint = videoPoints.querySelector('.active')
+		var nthPoint = videoPoints.querySelector('.point:nth-child(' + (this.activeVideo + 1) + ')')
+		activePoint && activePoint.classList.remove('active')
+		nthPoint && nthPoint.classList.add('active')
+
+		var slider = this
+
+		this.videoPlayer = new YT.Player('videoplayer', {
+			height: '503',
+			width: '670',
+			videoId: slider.videos[slider.activeVideo]
+		})
+	}
+}
