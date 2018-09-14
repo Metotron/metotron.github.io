f724fe92f6bb52ff81f84c7cbca89f500a5187f9
Metotron <metotron1@yahoo.com>
2018-09-14 19:30:46 +0300

    Продолжение разработки

--- skoda/js/skoda.js
+++ skoda/js/skoda.js
@@ -16,7 +16,7 @@ var videoSlider = {
 
 		leftArrow.addEventListener('click', function() { slider.slideTo(slider.activeVideo - 1) }, false)
 		rightArrow.addEventListener('click', function() { slider.slideTo(slider.activeVideo + 1) }, false)
-		
+
 		for (var pnt = 0; pnt < this.videos.length; ++pnt)
 			videoPoints.innerHTML += '<div class="point" data-index="' + pnt + '"></div>'
 
@@ -33,6 +33,8 @@ var videoSlider = {
 	slideTo: function(index) {
 		if (index < 0) index = this.videos.length - 1
 		if (index >= this.videos.length) index = 0
+
+		if (index == this.activeIndex) return 0
 
 		this.activeVideo = index
 		this.removePlayer()
@@ -67,24 +69,40 @@ var photoSlider = {
 	activeIndex: 0,
 	init: function() {
 		var leftArrow = document.querySelector('.photoslider .arrow.left')
-		var leftArrow = document.querySelector('.photoslider .arrow.right')
+		var rightArrow = document.querySelector('.photoslider .arrow.right')
 		var photoPoints = document.querySelector('.photopoints')
-		var slider = this
 
 		this.wrapper = document.querySelector('.photoslider .slider')
 		this.slidesCount = this.wrapper.querySelectorAll('.slide').length
 
 		for (var pnt = 0; pnt < this.slidesCount; ++pnt)
 			photoPoints.innerHTML += '<div class="point" data-index="' + pnt + '"></div>'
-		
+
+		for (var sld = 1; sld <= this.slidesCount; ++sld)
+			this.wrapper.querySelector('.slide:nth-child(' + sld + ')').setAttribute('data-index', sld - 2)
+		this.wrapper.querySelector('.slide:first-child').setAttribute('data-index', this.slidesCount - 1)
+
+		var slider = this
+		leftArrow.addEventListener('click', function() { slider.slideTo(slider.activeIndex - 1) }, false)
+		rightArrow.addEventListener('click', function() { slider.slideTo(slider.activeIndex + 1) }, false)
+
 		this.markActivePoint()
+
+		Array.from(photoPoints.querySelectorAll('.point')).forEach(function(point) {
+			point.addEventListener('click', function() {
+				var index = +this.getAttribute('data-index')
+				slider.slideTo(index)
+			}, false)
+		})
 	},
 
 	markActivePoint: function() {
 		var photoPoints = document.querySelector('.photopoints')
 		var activePoint = photoPoints.querySelector('.active')
 		activePoint && activePoint.classList.remove('active')
-		var newActive = photoPoints.querySelector('.point:nth-child(' + (this.activeIndex + 1) + ')')
+
+		var activeSlideIndex = +this.wrapper.querySelector('.slide:nth-child(2)').getAttribute('data-index')
+		var newActive = photoPoints.querySelector('.point:nth-child(' + (activeSlideIndex + 1) + ')')
 		newActive && newActive.classList.add('active')
 	},
 
@@ -92,7 +110,23 @@ var photoSlider = {
 		if (index < 0) index = this.slidesCount - 1
 		if (index >= this.slidesCount) index = 0
 
+		if (index == this.activeIndex) return 0
+
+		// Сперва ситуация, когда перематываем к слайду, который стоит первым в ряду (движение влево на один)
+		var firstSlide = this.wrapper.querySelector('.slide')
+		if (index == firstSlide.getAttribute('data-index')) {
+			var lastSlide = this.wrapper.querySelector('.slide:last-child')
+			this.wrapper.insertBefore(lastSlide, firstSlide)
+		}
+		else {
+			var slides = Arraw.from(this.wrapper.querySelectorAll('.slide'))
+			slides.forEach(function(slide) {
+				
+			})
+		}
+
 		this.activeIndex = index
-		
+		this.markActivePoint()
+
 	}
 }
